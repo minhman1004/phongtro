6835c8d684f63dd60411896372c86a0ae2867851
minhman10410 <14520511@gm.uit.edu.vn>
2018-10-04 01:06:16 +0700

    #13 Update Controllers

---
+++ application/controllers/AdminQuanly.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminQuanLy extends CI_Controller {
+	public function index() {
+		
+	}
+}
---
+++ application/controllers/PrimaryHeader.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PrimaryHeader extends CI_Controller {
+	public function index() {
+		
+	}
+}
