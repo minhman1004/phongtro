815852141364008150ccab049013896fae04db12
minhman10410 <14520511@gm.uit.edu.vn>
2018-10-11 22:42:34 +0700

    #Update File Structure

--- application/controllers/quan-ly/Dasboard.php
+++ application/controllers/quan-ly/Dasboard.php
@@ -4,5 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class AdminDasboard extends CI_Controller {
 	public function index() {
 		$this->load->view('primary/mainHeader');
+		$this->load->view('main/home/mainHome');
 	}
 }
